8505cdd3cd66f24f90806c37794085c7a9ea1287
nxtalievh <nataliehuynh128@gmail.com>
2025-02-11 17:19:17 -0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from fastai.vision.all import *
+
+def extract_number(path):
+    parts = str(path).split("/")
+    return parts[5]
+
+model = load_learner("flower_model.pkl")
+
+def predict(img):
+    img = PILImage.create(img_path)
+
+    result = model.predict(img_path)
+    print(result)
+
+    flower = result[0]
+    flower_index = result[1]
+    accuracy_list = result[2]
+
+    prediction_accuracy = accuracy_list[flower_index] * 100
+    if prediction_accuracy < 90:
+        image_title = f"I am not sure what flower this is. I am {prediction_accuracy:.3f}% sure this is a {flower} flower"
+    else:
+        image_title = f"{flower} - {prediction_accuracy:.3f}%"
+
+    img.show(title=image_title)
+
+st.title("Flower Prediction")
+st.text("Built by Natalie Huynh")
+
+upload_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+
+if upload_file is not None:
+    prediction = predict(upload_file)
+    st.image(upload_file, caption=prediction, use_column_width=True)
+    prediction = predict(upload_file)
+    st.write(prediction)
+
+
+
+
